a0d25396f91aef420fddc274e003184e8c66e9be
chadlohrli <chad.david.lohrli@gmail.com>
2022-06-25 22:53:28 -0400

    add calendar modal

--- frontend/src/Events.js
+++ frontend/src/Events.js
@@ -1,16 +1,29 @@
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Image from 'react-bootstrap/Image'
 
 import { useAccount, useContract, useSigner } from 'wagmi'
 import React, { useEffect, useState} from "react";
 import { toast } from 'react-toastify'
 
+// calendar
+import AddToCalendarHOC from 'react-add-to-calendar-hoc';
+
 const Events = () => {
 
   const { data: signer, isError, isSuccess } = useSigner()
   const { data: account } = useAccount()
+  const AddToCalendarDropdown = AddToCalendarHOC(Button, Dropdown);
 
   const [events, setEvents] = useState([])
+  const [currentEvent, setCurrentEvent] = useState({})
+
+  // modal hooks
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     console.log('signer success: ', isSuccess)
@@ -33,9 +46,23 @@ const Events = () => {
     console.log(index)
     const name = events[index].name
     toast(name)
+
+    // set current event
+
+    const event = {
+      description: 'Description of event. Going to have a lot of fun doing things that we scheduled ahead of time.',
+      duration: 1,
+      endDatetime: '20220619T220000Z',
+      location: 'NYC',
+      startDatetime: '20220619T210000Z',
+      title: 'Super Fun Event',
+    }
+    setCurrentEvent(event)
+    setShow(true)
   }
 
   return (
+    <>
     <Table responsive size="sm">
       <thead>
         <tr>
@@ -56,6 +83,29 @@ const Events = () => {
         ))}
       </tbody>
     </Table>
+
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <AddToCalendarDropdown
+          event={currentEvent}
+          linkProps={{
+            className: "link-info",
+          }}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+    </>
   )
 }
 
